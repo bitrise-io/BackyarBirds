09676c279548b8edc04d7d88cc3bc78340c53d9c
Ben Boral <ben.boral@bitrise.io>
2024-06-03 13:04:26 -0500

    use correct path and use Misc subdirectory

--- scripts/decrypt_signing_files.sh
+++ scripts/decrypt_signing_files.sh
@@ -4,4 +4,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Use the script directory as the base path
-gpg -o "$SCRIPT_DIR/Configuration/signing_files.zip" -d "$SCRIPT_DIR/Configuration/signing_files.gpg"
+gpg -o "$SCRIPT_DIR/../Misc/signing_files.zip" -d "$SCRIPT_DIR/../Misc/signing_files.gpg"
